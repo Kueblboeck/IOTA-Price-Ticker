53cb0773fd3aaef529a52ce0e30bb5803f374958
Sam Granger <sam@weprovide.com>
2017-08-19 00:11:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# IOTA Price Ticker for Chrome
+
+![screenshot](images/screenshot.png)
+
+## Install from source
+1. Clone or download this repo
+2. Navigate to chrome://extensions in Chrome
+3. Check the "Developer mode" checkbox (top right)
+4. "Click on Load unpacked extension..."
+5. Select the extension directory
+
+### Donations
+Feel like sending some lovin' :heart:? Donations can be sent to the following address.
+```WMFMLQ9VDUZBEYWKEGXZ9IRLZARGZHOYIDOOKAJS9MHFRDKNYYEIFILDDV9DDGHQZKZVVDUYXAMVSXPVDPYTXE9LKW```
---
+++ ticker.js
@@ -0,0 +1,20 @@
+var price = '...',
+request = {
+   "event":"subscribe",
+   "channel":"ticker",
+   "pair":"IOTUSD"
+};
+
+chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 255] });
+chrome.browserAction.setBadgeText({'text':price});
+var webSocket = $.simpleWebSocket({ url: 'wss://api.bitfinex.com/ws/2/' });
+
+webSocket.listen(function(message) {
+    if (message[1] !== "hb" && message.length === 2) {
+        price = message[1][6].toFixed(2);
+        chrome.browserAction.setBadgeText({ 'text': price });
+    }
+});
+
+webSocket.send(request).done(function() {
+}).fail(function(e) { });
